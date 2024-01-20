9c03e6fa4421f059abe886335861869a975178e5
bikaze <clmntmugisha@gmail.com>
2024-01-20 22:47:33 +0200

    checking newly pulled code

--- sort.h
+++ sort.h
@@ -20,5 +20,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void bubble_sort(int *array, size_t size);
-
+void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 #endif
