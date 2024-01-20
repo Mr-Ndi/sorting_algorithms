6df75548ed823d477f075af4076022fb57519242
bikaze <clmntmugisha@gmail.com>
2024-01-20 22:57:43 +0200

    checking newly pulled code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-		size_t i, j, min;
-		int swap;
+	size_t i, j, min;
+	int swap;
 
-		for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-				min = i;
-				for (j = i + 1; j < size; j++)
-				{
-						if (array[j] < array[min])
-								min = j;
-				}
-				if (min != i)
-				{
-						swap = array[i];
-						array[i] = array[min];
-						array[min] = swap;
-						print_array(array, size);
-				}
-		}
+			if (array[j] < array[min])
+				min = j;
+			}
+			if (min != i)
+			{
+				swap = array[i];
+				array[i] = array[min];
+				array[min] = swap;
+				print_array(array, size);
+			}
+	}
 }
