4dd405630c23349f2100c7e3675d1c16b49aa9a4
bikaze <clmntmugisha@gmail.com>
2024-01-21 12:17:50 +0200

    shell sort v1.0

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,27 +10,27 @@
  */
 void shell_sort(int *array, size_t size)
 {
-		size_t gap, i, j;
-		int temp;
+	size_t gap, i, j;
+	int temp;
 
-		if (array == NULL || size < 2)
-				return;
-		gap = 1;
-		while (gap < size / 3)
-				gap = gap * 3 + 1;
-		while (gap > 0)
+	if (array == NULL || size < 2)
+		return;
+	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
 		{
-				for (i = gap; i < size; i++)
-				{
-						temp = array[i];
-						for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-						{
-								array[j] = array[j - gap];
-						}
-						array[j] = temp;
-				}
-				print_array(array, size);
-				gap = (gap - 1) / 3;
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
 		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }
 
--- sort.h
+++ sort.h
@@ -23,4 +23,5 @@ void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
